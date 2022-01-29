ba8ef9b41f0632abd416f3edaa5286daf43e73df
Agustin <agustin.utn888@gmail.com>
2022-01-28 22:59:55 -0300

    g2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,11 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("okas");
+let entradaDatos;
+entradaDatos = prompt('Ingresar nombre');
+while(isNaN(entradaDatos)==false){
+	entradaDatos = prompt('Error re-ingresar nombre');
+}
+	alert(entradaDatos);
 }
 
